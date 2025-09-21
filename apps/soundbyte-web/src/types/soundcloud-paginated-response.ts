2393f054e6e89066c75b776de885c70c889ec868
JackDiSalvatore <jackp.disalvatore@gmail.com>
2025-09-21 11:13:47 -0500

    updating
    Signed-off-by: JackDiSalvatore <jackp.disalvatore@gmail.com>

--- apps/soundbyte-web/src/types/soundcloud-paginated-response.ts
+++ apps/soundbyte-web/src/types/soundcloud-paginated-response.ts
@@ -1,4 +1,4 @@
 export type SoundCloudPaginatedResponse<T> = {
   collection: T;
-  next_href: string; //https://api.soundcloud.com/me/likes/tracks?cursor=2024-06-01T16%3A23%3A38.001Z%2Cuser-track-likes%2C000-00000000000147753238-00000000000213040837&linked_partitioning=true&page_size=10
+  next_href?: string; // optional cursor URL from SoundCloud
 };
